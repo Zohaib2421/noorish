ae68552149842dab7d3266509c0f2672a9d32fd4
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-07-08 16:10:57 -0500

    Button is in a better location.

--- src/components/noorishButton.jsx
+++ src/components/noorishButton.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function NoorishButton({ text, link, textColor, backgroundColor, borderColor, borderStrength }) {
     return (
-        <button className='flex rounded-3xl bg-noorishGreen text-almostWhite px-4 py-4 border'> {text} </button>
+        <button className='flex rounded-full text-lg ml-40 mt-96 bg-noorishGreen text-almostWhite px-10 py-5 border'> {text} </button>
     )
 }
--- src/hero/navbar.jsx
+++ src/hero/navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <div className='flex bg-noorishGreen justify-evenly min-w-full h-fit font-outfit text-white mt'>
-                <a href='' className='mt-5'>HOME</a>
+                <a href='' className='mt-5 text-sm'>HOME</a>
                 <a href='' className='mt-5'>RESEARCH</a>
                 <a href='' className='mt-5'>RECOMMENDATIONS</a>
                 <a href='' className='mt-5'>ABOUT</a>
