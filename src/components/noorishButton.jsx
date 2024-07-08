cdc633207417e49a4d0d9535f94dd5e7da6874bc
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-07-08 16:36:21 -0500

    conditional if the border props should be included in the classname so that they can be added for tailwind styling.

--- src/components/noorishButton.jsx
+++ src/components/noorishButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function NoorishButton({ text, link, textColor, backgroundColor, borderColor, borderStrength }) {
+export default function NoorishButton({ text, link, textColor, backgroundColor, borderColor = 'none', borderStrength = 'none' }) {
     const handleClick = () => { window.location.href = link }
-
+    const border = borderStrength === 'none' ? '' : `${borderStrength} solid ${borderColor}`
     return (
-        <button className={`flex rounded-full text-lg ml-40 mt-96 font-outfit font-medium bg-${backgroundColor} text-${textColor} px-10 py-5 border`}
+        <button className={`flex rounded-full text-lg ml-40 mt-96 font-outfit font-medium bg-${backgroundColor} text-${textColor} px-10 py-5`}
         onClick={handleClick}> {text} </button>
     )
 }
