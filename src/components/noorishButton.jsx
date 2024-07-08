389a267329060aec8e47bb7d637c1929dcb2462f
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-07-08 16:22:55 -0500

    Button has the correct styling

--- src/components/noorishButton.jsx
+++ src/components/noorishButton.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function NoorishButton({ text, link, textColor, backgroundColor, borderColor, borderStrength }) {
     return (
-        <button className='flex rounded-full text-lg ml-40 mt-96 bg-noorishGreen text-almostWhite px-10 py-5 border'> {text} </button>
+        <button className='flex rounded-full text-lg ml-40 mt-96 font-outfit font-medium bg-almostWhite text-buttonTextColor px-10 py-5 border'> {text} </button>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,21 @@ module.exports = {
     extend: {
       fontFamily:
       {
-        'outfit': ['Outfit']
+        'outfit': ['Outfit'],
+        'outfit-bold': ['Outfit-Bold']
+      },
+      fontWeight:
+      {
+        'medium': 400,
+        'semibold': 600,
+        'bold': 700,
+        'extra-bold': 800
       },
       colors: 
       {
         noorishGreen: '#19251C',
         almostWhite: '#FFFBEB',
+        buttonTextColor: '#73592F'
       }
     },
   },
